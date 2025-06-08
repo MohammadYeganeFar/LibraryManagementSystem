5961814f9b13b2a0db2e68b73082f783faeff9cc
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-06-08 23:24:39 +0330

    add BorrowedBookAdmin admin and fix AuthorAdmin

--- library/admin.py
+++ library/admin.py
@@ -7,13 +7,20 @@ class MemberAdmin(admin.ModelAdmin):
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['name']
-    filter_horizontal = ['books']
 
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'ISBN']
     filter_horizontal = ['authors']
 
+
+class BorrowedBookAdmin(admin.ModelAdmin):
+    list_display = ['book', 'member', 'loan_date', 'return_date']
+    date_hierarchy = 'loan_date'
+    search_fields = ['book', 'member']
+
+
 admin.site.register(lib_models.Book, BookAdmin)
 admin.site.register(lib_models.Author, AuthorAdmin)
 admin.site.register(lib_models.Member, MemberAdmin)
+admin.site.register(lib_models.BorrowedBook, BorrowedBookAdmin)
