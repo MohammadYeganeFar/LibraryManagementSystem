1a6a1098da579d19e067ee6c0daaaa9b25357cc8
mmd on laptop <mmdvafaj@gmail.com>
2025-06-08 14:00:16 +0430

    initial admins

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from library import models as lib_models
 
-# Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+    search_fields = ['name',]
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    filter_horizontal = ['books']
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'ISBN']
+    filter_horizontal = ['authors']
+
+admin.site.register(lib_models.Book, BookAdmin)
+admin.site.register(lib_models.Author, AuthorAdmin)
+admin.site.register(lib_models.Member, MemberAdmin)
