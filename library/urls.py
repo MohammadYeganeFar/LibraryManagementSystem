66385125f4799ea2e66c603c1f0f5f11134def7c
mmd on laptop <mmdvafaj@gmail.com>
2025-06-10 14:20:36 +0430

    add suffix for urlpatterns

--- library/urls.py
+++ library/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from library import views
 
 
@@ -6,3 +7,5 @@ urlpatterns = [
     path('members/', views.member_list, name='member_list'),
     path('members/<int:pk>/', views.member_detail, name='member_dateil')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
