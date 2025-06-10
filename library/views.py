cc2b50815029c3a9721abc13e0d26df2cb93107c
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-06-10 13:19:59 +0330

    add member list view and urls

--- library/views.py
+++ library/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from library import models as lib_models
+from library import serializers
 
-# Create your views here.
+@csrf_exempt
+def member_list(request):
+    if request.method == 'GET':
+        members = lib_models.Member.objects.all()
+        serializer = serializers.MemberSerializer(members, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        print(f'request: {request}, type: {type(request)}')
+        data = JSONParser().parse(request)
+        serializer = serializers.MemberSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+        
