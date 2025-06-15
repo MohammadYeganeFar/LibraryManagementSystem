4cab014a74342c77c4d1326d24c2bbc7377e9005
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-06-15 20:22:12 +0330

    using router instead of normal url mapping

--- library/urls.py
+++ library/urls.py
@@ -1,44 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from library.views import MemberViewSet, AuthorViewSet
 
 
-member_list = MemberViewSet.as_view(
-    {
-        'get': 'list',
-        'post': 'create',
-    }
-)
-member_detail = MemberViewSet.as_view(
-    {
-        'get':'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    }
-)
-member_name = MemberViewSet.as_view(
-    {
-        'get': 'fullname'
-    }
-)
-author_list = AuthorViewSet.as_view(
-    {
-        'get': 'list'
-    }
-)
-author_detail = AuthorViewSet.as_view(
-    {
-        'get': 'retrieve'
-    }
-)
-
+router = DefaultRouter()
+router.register(r'test-members', MemberViewSet, basename='member')
+router.register(r'authors', AuthorViewSet, basename='author')
+# prefix is for URL mapping and
+# base name is for naming the urls.(name=member-list)
 urlpatterns = [
-    path('members/', member_list, name='member-list'),
-    path('members/<int:pk>/', member_detail, name='member-detail'),
-    path('members/<int:pk>/name/', member_name, name='member-name'),
-    path('authors/', author_list, name='author-list'),
-    path('authors/<int:pk>/', member_detail, name='author-detail'),
+    path('', include(router.urls))
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
